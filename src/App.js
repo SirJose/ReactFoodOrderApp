a7a76f0a05623ecda68c751402956f5ed66de927
josecl@galileo.edu <josecl@galileo.edu>
2022-05-04 12:00:44 -0600

    Crear cart modal

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
@@ -5,6 +6,7 @@ import Meals from "./components/Meals/Meals";
 const App = () => {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
