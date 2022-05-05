02699a1497d3ecf56ab53c19620cb6e39eccbd56
josecl@galileo.edu <josecl@galileo.edu>
2022-05-05 12:47:09 -0600

    Add items to cart (counter)

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,25 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = props => {
 
+    const [amountIsValid, setAmountIsValid] = useState(true);
+
     const amountInputRef = useRef();
 
     const submitHandler = event => {
         event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5 ){
+            setAmountIsValid(false);
+            return;
+        }
+        setAmountIsValid(true);
+        props.onAddToCart(enteredAmountNumber);
     };
 
     return (
@@ -24,6 +36,7 @@ const MealItemForm = props => {
                     defaultValue: '1'
                 }} />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
         </form>
     );
 }
