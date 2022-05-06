fdc6a658f2b88ae9f8773aafe12bd1ea3dc5e2a1
JoseCL <josecl@galileo.edu>
2022-05-05 21:14:26 -0600

    Add item to cart animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = props => {
 
+    const [btnHighlight, setBtnHighlight] = useState(false);
+
     const cartCtx = useContext(CartContext);
 
     const numberOfCartItems = cartCtx.items.reduce((a,b) => a + b.amount, 0);
 
+    const btnClasses = `${classes.button} ${btnHighlight ? classes.bump:''}`;
+
+    useEffect(() => {
+        if(cartCtx.items.length === 0){
+            return ;
+        }
+        setBtnHighlight(true);
+
+        const timer = setTimeout(() => {
+            setBtnHighlight(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [cartCtx.items]);
+    
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span>
                 <CartIcon className={classes.icon} />
             </span>
